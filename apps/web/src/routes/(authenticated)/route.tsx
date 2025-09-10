7c399f300fd6de2376334c411f04ae213f659000
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-09-10 13:53:34 +0530

    fix authenticated route

--- apps/web/src/routes/(authenticated)/route.tsx
+++ apps/web/src/routes/(authenticated)/route.tsx
@@ -1,20 +1,20 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { AdminHeader, SideNav } from "@/components/admin/dashboard/sidebar-nav";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(authenticated)")({
+  beforeLoad: ({ context }) => {
+    const session = context.session;
+
+    if (!session) {
+      throw redirect({
+        to: "/",
+      });
+    }
+
+    return { session };
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <SideNav />
-      <div className="flex flex-col">
-        <AdminHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
+  return <Outlet />;
 }
